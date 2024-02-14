f11018913529b31fe0095f9cb8453025719d3245
Dion VH <91065539+pgm-dionvanh@users.noreply.github.com>
2024-02-14 11:48:56 +0100

    Add taskList, get & delete

--- TaskManagement/Controllers/TasksController.cs
+++ TaskManagement/Controllers/TasksController.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TaskManagement.Controllers
 {
@@ -15,5 +16,41 @@ namespace TaskManagement.Controllers
         }
 
         /* Todo write crud */
+
+        //Task list
+        private static List<Task> _tasks = new List<Task>
+        {
+            new Task { Id = 1, Title = "Task 1", Description = "Description for Task 1", DueDate = "2024-02-14" },
+            new Task { Id = 2, Title = "Task 2", Description = "Description for Task 2", DueDate = "2024-02-15" },
+            new Task { Id = 3, Title = "Task 3", Description = "Description for Task 3", DueDate = "2024-02-16" }
+        };
+
+        // GET: api/tasks
+        [HttpGet(Name = "GetTasks")]
+        public ActionResult<IEnumerable<Task>> GetTasks()
+        {
+            /* Return all tasks */
+            return _tasks;
+        }
+
+        // Delete: api/tasks/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTask(int id)
+        {
+            /* Find task by id */
+            var taskToRemove = _tasks.SingleOrDefault(t => t.Id == id);
+
+           /* if task not found */
+           if(taskToRemove == null) return NotFound();
+
+
+
+           /* Remove task */
+           _tasks.Remove(taskToRemove);
+
+
+            /* Return 200 ok status code */
+            return Ok();
+        }
     }
 }
---
+++ TaskManagement/Task.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManagement
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string DueDate { get; set; }
+    }
+}
